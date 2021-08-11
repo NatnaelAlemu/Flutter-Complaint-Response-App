8dd0df5f35531b21b4c255fccdf16772f1c3279f
Natnael Alemu <It.natnaelalemu@gmail.com>
2021-08-12 00:09:56 +0300

    Creating intial folder structure for backend

---
+++ backend/config/databaseConfig.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+export const dbConfig = () => {
+    mongoose.connect(process.env.DATABASE_URI, {
+        useNewUrlParser: true,
+    })
+    const connection = mongoose.connection
+    connection.on('error',(err)=>console.log(err.message))
+    connection.once('open',()=>console.log('Database connection success'))
+    
+}

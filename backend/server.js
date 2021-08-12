a1850fa3268caecf9936ff064373b9d79daa41f6
Natnael Alemu <It.natnaelalemu@gmail.com>
2021-08-12 05:10:57 +0300

    creating the app server and testing the api  on postman

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import Router from './routes/auth.js'
+import {dbConfig} from './config/databaseConfig.js'
+dotenv.config()
+const app = express();
+app.use(express.json())
+dbConfig()
+
+app.use('/api',Router)
+
+app.listen(process.env.PORT,()=>console.log(`Server is listening at http://localhost://${process.env.PORT}`))

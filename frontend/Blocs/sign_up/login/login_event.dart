cb6c2060528338e0906a07f031f0fca52293070c
Mechael Solomon <it.michael.teshome@gmail.com>
2021-09-08 00:07:27 +0300

    creating loginbloc to resolve #2

---
+++ frontend/Blocs/sign_up/login/login_event.dart
@@ -0,0 +1,12 @@
+part of 'login_bloc.dart';
+
+@immutable
+abstract class LoginEvent {}
+class Login extends LoginEvent {
+  final LoginModel loginModel;
+  Login({
+    required this.loginModel,
+  });
+
+
+}

78d24f85ff2bb5be2dd363a8aba835d342a6af8f
Feven G/Tensay <Astfevgten@gmail.com>
2021-09-11 14:16:25 +0300

    update on backend middlewares

--- backend/middlewares/authmiddleware.js
+++ backend/middlewares/authmiddleware.js
@@ -2,23 +2,29 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config()
 export const authMiddleWare = (req, res, next) => {
+    console.log("some request is commig");
     const tokenHeader = req.headers.authorization
+    console.log(tokenHeader)
     const token = tokenHeader && tokenHeader.split(' ')[1]
+    console.log(`token ${token}`)
     if (token) {
-        jwt.verify(
+        jwt.verify( 
             token,
             process.env.ACCESS_TOKEN_SECRET,
             (err, authData) => {
                 if (err) {
+                    console.log('error')
+                    console.log(err.message)
                     return res.status(403).send(err.message)
                 }
                 else {
+                    console.log(`user ${authData}`)
                     res.user = authData
                     next()
                 }
             }
         )
     } else {
-        res.status(403).send("can't get token")
+        res.status(403).json({"message":"can't get token"})
     }
 }
